e3f2587fa4e35a3510cf5df6978db7990b19e4f0
mahdistt <80247767+mahdistt@users.noreply.github.com>
2021-07-29 22:58:39 +0330

    postgresql setting in windows

--- CrmProject/settings.py
+++ CrmProject/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'CrmProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maktab_blog',
+        'USER': 'usr_blog',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
