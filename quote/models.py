16c1df961148f0631bf4bdd57c82ee026094a8b8
mahdistt <80247767+mahdistt@users.noreply.github.com>
2021-08-03 20:31:23 +0330

    WIP:Calculate requirement quote (taxation,discount,price)

--- quote/models.py
+++ quote/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Sum, F
 
 from organization.models import OrganizationInfo
 from product.models import ProductInfo
@@ -16,6 +17,29 @@ class Quote(models.Model):
     def __str__(self):
         return self.organization_related
 
+    def get_quantity(self):
+        return self.quoteitem_set.all().aggregate(Sum('quantity')).get('quantity__sum', 0)
+
+    def get_total_quote_price(self):
+        """
+        Total price without taxation and discount
+        """
+        return self.quoteitem_set.all().annotate(
+            total_quote_price=F('price') * F('price')).aggregate(
+            Sum('total_start_price'))['total_start_price__sum']
+
+    def get_quote_discount(self):
+        return self.quoteitem_set.all().annotate(
+            total_quote_price=F('price') * F('quantity')).annotate(
+            discount=((F('total_quote_price') * F('discount')) / 100)).aggregate(
+            Sum('discount'))['discount__sum']
+
+    def get_quote_taxation(self):
+        """
+        Calculate 9% taxation
+        """
+        pass
+
 
 class QuoteItem(models.Model):
     """
@@ -29,3 +53,6 @@ class QuoteItem(models.Model):
 
     def __str__(self):
         return self.quote
+
+    def get_total_price(self):
+        return self.price * self.quantity
